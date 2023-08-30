fe4602b6217764e1ec8f2b1c154263306ebbf714
Lilly Hewitt <70710764+lillyhewitt@users.noreply.github.com>
2023-08-30 09:40:26 -0400

    Create randChar.c

---
+++ randChar.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+//function randchar() is a character function that returns a random character from ‘A’ – ‘Z’. 
+//The character is used in the main program to generate a random 7 letter word
+char randchar() {
+    // returns 'a' char just to make sure function is working
+    return 'a';
+}
