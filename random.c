1539c0d8b3d09199eb50c785fa3cf1673dcf5549
lillyhewitt <tuo47895@temple.edu>
2023-08-30 10:20:46 -0400

    simplifies function randomchar()

--- random.c
+++ random.c
@@ -5,10 +5,8 @@
 //function randchar() is a character function that returns a random character from ‘A’ – ‘Z’. 
 //The character is used in the main program to generate a random 7 letter word
 char randchar() {
-    // creates variable to save random character to 
-    char randomChar;
-    // use ASCII values to grab random character between A (65) - Z (90)
-    randomChar = (rand() % (90 - 65)) + 65;
-    // return char
+    // assigns an ASCII values between A (65) - Z (90) to variable randomChar
+    char randomChar = (rand() % (90 - 65)) + 65;
+    // returns randomChar
     return randomChar;
 }
